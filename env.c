c7b4aff9d6f56cc293348508f2a163452979360f
Todd C. Miller <Todd.Miller@courtesan.com>
2010-06-25 13:39:21 -0400

    In unsetenv(), do not assign ep early as we may end up reallocating env.envp which could result in ep pointing to freed memory if the environ pointer is out of sync with env.envp.

--- env.c
+++ env.c
@@ -317,7 +317,7 @@ int
 unsetenv(var)
     const char *var;
 {
-    char **ep = env.envp;
+    char **ep;
     size_t len;
 
     if (strchr(var, '=') != NULL) {
@@ -355,7 +355,7 @@ unsetenv(var)
     }
 
     len = strlen(var);
-    while (*ep != NULL) {
+    for (ep = env.envp; *ep != NULL;) {
 	if (strncmp(var, *ep, len) == 0 && (*ep)[len] == '=') {
 	    /* Found it; shift remainder + NULL over by one and update len. */
 	    memmove(ep, ep + 1,
